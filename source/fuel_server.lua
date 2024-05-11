faa5f4e16dcd1926aeb3b6ca75972d71469468e9
vames-dev <60104107+vames-dev@users.noreply.github.com>
2024-05-11 22:51:56 +0200

    Update fuel_server.lua

--- source/fuel_server.lua
+++ source/fuel_server.lua
@@ -11,5 +11,5 @@ end)
 
 RegisterServerEvent('fuel:fuelHasBeenStealed')
 AddEventHandler('fuel:fuelHasBeenStealed', function(storeId, plate, fuelCount)
-	exports.vms_stores:sendAnnouncement(source, storeId, (Config.Strings.StealedFuel):format(plate, fuelCount), 'monitoring')
+	exports.vms_stores:sendAnnouncement(source, storeId, (Config.Strings.StealedFuel):format(plate, fuelCount), 'monitoring', {fuelCount = fuelCount})
 end)
