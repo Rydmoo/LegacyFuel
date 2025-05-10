6799aac71f1fbe6854187a467a25782d171c81a9
Rydmo <120792919+Rydmoo@users.noreply.github.com>
2025-05-10 18:40:32 +0200

    Update fuel_server.lua

--- source/fuel_server.lua
+++ source/fuel_server.lua
@@ -1,15 +1,10 @@
-local ESX = exports['es_extended']:getSharedObject()
-
-RegisterServerEvent('fuel:pay')
-AddEventHandler('fuel:pay', function(price)
-	local xPlayer = ESX.GetPlayerFromId(source)
-	local amount = ESX.Math.Round(price)
-	if price > 0 then
-		xPlayer.removeMoney(amount)
-	end
-end)
-
 RegisterServerEvent('fuel:fuelHasBeenStealed')
 AddEventHandler('fuel:fuelHasBeenStealed', function(storeId, plate, fuelCount)
-	exports.vms_stores:sendAnnouncement(source, storeId, (Config.Strings.StealedFuel):format(plate, fuelCount), 'monitoring', {fuelCount = fuelCount})
+    exports.vms_stores:sendAnnouncement(
+        source,
+        storeId,
+        (Config.Strings.StealedFuel):format(plate, fuelCount),
+        'monitoring',
+        { fuelCount = fuelCount }
+    )
 end)
